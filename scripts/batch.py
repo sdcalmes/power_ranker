7456f6c5e02512396482980e91acd282022c57b8
Sam Calmes <samuel.calmes@kohls.com>
2018-08-31 10:33:00 -0500

    batch now an interactive process, much more streamlined

--- scripts/batch.py
+++ scripts/batch.py
@@ -1,6 +1,7 @@
 import subprocess
 from tempfile import mkstemp
 from shutil import move
+import datetime
 from os import fdopen, remove
 
 def replace(file_path, pattern, subst):
@@ -34,55 +35,24 @@ path = 'MY_LOCAL_CONFIG.cfg'
 #     replace(path, 'week = ' + str(x-1), 'week = ' + str(x))
 #     subprocess.call("python command_line.py -c MY_LOCAL_CONFIG.cfg")
 
-subprocess.call('python command_line.py -l 185829 -y 2015 -w 1')
-replace(path, 'doPlayoffs      = False', 'doPlayoffs = False')
+print("This batch script will pull all data from your league. It only needs to be ran once.")
+year = int(input("What year whould you like to start with? "))
+leagueId = input("What is your league ID? ")
+currYear = int(datetime.datetime.now().year)
 
-for x in range(2, 14):
-    if x == 2:
+for y in range(year, currYear):
+    subprocess.call('python command_line.py -l ' + leagueId + ' -y' + str(y) + ' -w 1')
+    replace(path, 'doPlayoffs      = False', 'doPlayoffs = False')
+
+    for x in range(2, 14):
+      if x == 2:
         replace(path, 'doPlayoffs = False', 'doPlayoffs = True')
         replace(path, 'num_simulations = 200000', 'num_simulations = 20000')
-    if x == 5:
+      if x == 5:
         replace(path, 'num_simulations = 20000', 'num_simulations = 60000')
-    if x == 8:
+      if x == 8:
         replace(path, 'num_simulations = 60000', 'num_simulations = 100000')
 
-    print('Year: 2015, Week: %s' % x)
-    replace(path, 'week = ' + str(x-1), 'week = ' + str(x))
-    subprocess.call("python command_line.py -c MY_LOCAL_CONFIG.cfg")
-
-subprocess.call('python command_line.py -l 185829 -y 2016 -w 1')
-replace(path, 'doPlayoffs      = False', 'doPlayoffs = False')
-
-for x in range(2, 14):
-    if x == 2:
-        print('X is 2')
-        replace(path, 'doPlayoffs = False', 'doPlayoffs = True')
-        replace(path, 'num_simulations = 200000', 'num_simulations = 20000')
-    if x == 5:
-        replace(path, 'num_simulations = 20000', 'num_simulations = 60000')
-    if x == 8:
-        replace(path, 'num_simulations = 60000', 'num_simulations = 100000')
-
-    print('Year: 2016, Week: %s' % x)
-    replace(path, 'week = ' + str(x-1), 'week = ' + str(x))
-    subprocess.call("python command_line.py -c MY_LOCAL_CONFIG.cfg")
-
-subprocess.call('python command_line.py -l 185829 -y 2017 -w 1')
-replace(path, 'doPlayoffs      = False', 'doPlayoffs = False')
-
-for x in range(2, 14):
-    if x == 2:
-        replace(path, 'doPlayoffs = False', 'doPlayoffs = True')
-        replace(path, 'num_simulations = 200000', 'num_simulations = 20000')
-    if x == 5:
-        replace(path, 'num_simulations = 20000', 'num_simulations = 60000')
-    if x == 8:
-        replace(path, 'num_simulations = 60000', 'num_simulations = 100000')
-
-    print('Year: 2017, Week: %s' % x)
-    replace(path, 'week = ' + str(x-1), 'week = ' + str(x))
-    subprocess.call("python command_line.py -c MY_LOCAL_CONFIG.cfg")
-
-
-
-
+      print('Year: %s, Week: %s' % (y, x))
+      replace(path, 'week = ' + str(x-1), 'week = ' + str(x))
+      subprocess.call("python command_line.py -c MY_LOCAL_CONFIG.cfg")
